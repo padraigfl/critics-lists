3d11e53633fb15dbc5cf8df1ca53bf06ecce7aa2
padraigfl <padraigflood1@gmail.com>
2020-05-23 16:07:50 +0100

    updated landing a wee bit

---
+++ src/Technical.svelte
@@ -0,0 +1,11 @@
+
+<div class="Landing">
+  <h1>Technical Stuff (to be filled in later)</h1>
+
+  <h2>Stack</h2>
+  <p>
+    This is the first thing I ever done with Svelte and I largely built it offline with a limited doc set... I'd say it's pretty obvious I didn't have a clue what I was at in some parts so if something looks weird that'll be why.
+  </p>
+  <p>OMDb was used to build up the data</p>
+  <p>JS-DOM was used to scrape data from the metacritic pages, some fairly messy node scripts formatted it</p>
+</div>
--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -6,48 +6,86 @@
   <title>Critics Lists: Intro</title>
 </svelte:head>
 <div class="Landing">
-	<p>This site collects data from a whole load of year end lists from over the last decade for the following purposes:</p>
+	<h1>Aggregated End-of-Year Lists 2010-2010s</h1>
 
+	<h2>What does it do?</h2>
+	<p>
+		Metacritic gathers up all these lists every year and makes a pretty basic top 20 out of it.
+		This site presents a longer list from the same data with some additional data points (film only) to make it easier to unearth some gems.
+		There's some basic means of sorting and filtering results too so you can find stuff for a particular mood more quickly.
+	</p>
+
+	<h2>Features</h2>
+
+	<ol>
+		<li>Year Lists aggregated using a custom weighting scheme (#1 gets 10 points, #2 gets 9, etc), this will be customisable in the future</li>
+		<li>Genre, country and language filters</li>
+		<li>Sorting options</li>
+		<li>A custom list for each format to track items you've flagged, which is then viewable at <a href="/#/interested/film">Film</a>, <a href="/#/interested/tv">TV</a> and <a href="/#/interested/album">Music</a></li>
+		<li>Three customisable filter options to reduce list size (options are icon based, other than the list one you can interpret them as you like)</li>
+		<li>Some generated factoids about the content of the lists, I don't think I've gotten much of interest from these tbh</li>
+	</ol>
+
+	<h2>Motive</h2>
+	<p>
+		If you're a pretty obsessive film snob you probably know the bulk of stuff in Metacritic's aggregated top 20, and have probably seen most of them.
+		The really interesting stuff are the things that maybe only 3 critics even saw yet one of them deemed worthy of their
+		top 10, hidden away on Rotten Tomatoes with a 90% average from 10 reviews, and on page 6 of Letterbox'd by the overload of very softly graded
+		music docs.
+	</p>
+	<p>
+		Each year I'd trawl through endless top 10s looking for names I didn't recognise, Googling every one and usually unearthing a couple of neat indies at thevery least...
+		It was pretty tedious stuff when you could dismiss half of them with just a few other data points
+		(e.g. by a filmmaker I don't like; or had already dismissed but forgotten the name). Finding it increasingly hard to
+		find new interesting things to watch and having to trawl through all the noise in lists on Letterbox'd, I decided to try
+		and make something from this data on metacritic aggregated with a movie database
+	</p>
+	<p>...and, er, I also wanted to see how far I could push a 2 color style (#eeeeee and #111111)</p>
+
+	<h2>Still to do</h2>
 	<ul>
-		<li>Gathering and presenting additional data than has been provided by other aggregators</li>
-		<li>Allow custom rating metrics and modifications (e.g. omitting lists)</li>
-		<li>Quick searching of the listed entries (links only lead to searches on other sites, not direct links, sorry...)</li>
-		<li>Make it easier for me to scroll down through it for some oddities</li>
-		<li>Excuse to use Svelte kinda recklessly (I've done about 95% of this offline so been kinda guessing stuff from READMEs and the library source code)</li>
+		<li>Multiple points weighting systems</li>
+		<li>Aggregation of the individual year lists (would be a very large file download)</li>
+		<li>Extra data for TV shows</li>
+		<li>Style improvements (e.g. cleaner navigation)</li>
+		<li>Performance issue resolutions</li>
+		<li>Data fixes</li>
 	</ul>
 
-	<p>
-		With respect to the data gathering, I'm mostly making it up as a go along.
-		The key one I want to do is a an aggregated list of all the end of year lists to compare directly with the decade list, I also might try and weigh things in different ways (e.g. average out critics from publications with more than one list)
-		As it stands there are a few metrics pulled from each list which I found interesting:
-	</p>
-
+	<h2>Things I won't do</h2>
 	<ul>
-		<li><strong># Unique Entries</strong> Obvious enough?</li>
-		<li><strong>Highest ranked with no #</strong> Seems like a solid bet for an easy pleaser that potentially has slipped a bit under the radar? Alternatively it's just something people like to include in their list</li>
-		<li><strong>Lowest ranked with a #1</strong> includes a validator for entries that only make a few lists. Not sure there's any one way to interpret this one.</li>
-		<li><strong>Highest ranked pair that share no list</strong> Just a kinda cool contrast, potentially a means of signalling the biggest split in trends with mainstream critics</li>
-		<li><strong>Most contrarian critic</strong> Person who's list matches up least with the overall weighted list, not sure there's anything of value from this, contains a validator for people who have totally obscure lists</li>
+		<li>Album data: the hassle in sanitizing the data would be absolutely huge due</li>
+		<li>Display images on load: would involve loading so many assets at once</li>
+		<li>Save lists between devices: using local storage of browser, there is no server involved</li>
+		<li>Up to date data: Again, due to no server being involved. Although I might be able to throw together a script to occasionally update this.</li>
 	</ul>
 
-	<p>
-		Other suggestions are extremely welcome, bear in mind that I only have access to the names and their rankings on lists here at the moment.<br/>
-		Some of the data points could undoubtedly be cleaned up but I have no real interest in trying to (e.g. "<em>[Rock list]View metal albums list [not included in combined standings]</em>" is obviously not a critic)
-	</p>
+	<h2>Sources</h2>
 
-	<p>
-		Lists display amount of number ones, amount of mentions, amount of awards won and nominations without an award.
-	</p>
+	<p>The full collection of lists available are</p>
+	{#each OPTIONS.formats as format}
+		<div class="yearList">
+			<h3>{format.toUpperCase()}</h3>
+			<ul >
+				{#each OPTIONS.years as year}
+					<li>
+						<a class="yearLink" href={`/#/${format}/${year}`}>
+							{year}
+						</a> – [<a href={`/${format}/${year}.json`}>JSON</a>,
+						<a href={SOURCE[format][year]} target="_blank">Source</a>]
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/each}
 
-	<p>
-		Film data is pulled from OMDb, some of it is going to be inaccurate and it was not all grabbed at the same time. It's mostly about giving some extra info without having to search.
-	</p>
-
+	<h2>Bye</h2>
 	<p>
 		Oh, and sorry about the obnoxious marquee element on the side. If you're on mobile you cant see it so here are the links.
 		{#each [
 			{ link: 'github.com/padraigfl', text: 'Github' },
 			{ link: 'github.com/padraigfl/critic-lists', text: 'Source code'},
+			{ link: 'dvd-rom.netlify.app', text: 'DVD Menu Simulator' },
 			{ link: 'packard-belle.netlify.com', text: 'Windows98 Clone' },
 			{ link: 'react-coursebuilder.netlify.com', text: 'Youtube App thing' },
 		] as {link, text}, i}
@@ -58,23 +96,5 @@
 		{/each}
 	</p>
 
-	<p>Also sorry about the styles, I was mostly just messing around with stuff to see what I could make work, wanted to limit myself to two colors as well, for no reason whatsoever.</p>
-
-
-	<div>
-		The full collection of lists available are
-		{#each OPTIONS.formats as format}
-			<p>{format.toUpperCase()}</p>
-			<ul>
-				{#each OPTIONS.years as year}
-					<li>
-						<a  tyle={{ fontVariantNumeric: 'tabular-number' }} href={`/#/${format}/${year}`}>
-							{year}
-						</a> – [<a href={`/${format}/${year}.json`}>JSON</a>,
-						<a href={SOURCE[format][year]} target="_blank">Source</a>]
-					</li>
-				{/each}
-			</ul>
-		{/each}
-	</div>
+	<p>If you're actually using this and have some feedback, I'd love to hear it, try my Github ^</p>
 </div>
